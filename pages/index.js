5daf01280b1ff548d62d392ca6953ae859d55326
cmoll-nicresource <cmoll@nicotineresource.org>
2025-01-24 16:15:45 -0500

    Added list of properties

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,15 @@ export default function Home() {
         <Header title="Nicotine Resource Consortium" />
         <p className="description">
           The Nicotine Resource Consortium is a 501(c)(6) nonprofit dedicated to informing and convening all stakeholders in the tobacco and nicotine industries.
+          Please use the following links to access the Consortium's various properties:
         </p>
+        <ul>
+          <li><a href="https://www.nicotine360.org">Nicotine360</li>
+          <li><a href="https://www.atnf.forum">American Tobacco & Nicotine Forum (ATNF)</li>
+          <li><a href="https://www.gtnf.org">Global Tobacco & Nicotine Forum (GTNF)</li>
+          <li><a href="https://tobaccoreporter.com">Tobacco Reporter</li>
+          <li><a href="https://industryguide.org">The Industry Guide</li>
+        </ul>
       </main>
 
       <Footer />
