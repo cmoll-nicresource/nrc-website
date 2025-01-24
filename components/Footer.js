eff8ecfa26760e4efb28652432126646f4b278d1
cmoll-nicresource <cmoll@nicotineresource.org>
2025-01-24 16:10:38 -0500

    Added logo to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/NRC@4x.png" alt="Nicotine Resource Consortium Logo" className={styles.logo} />
       </footer>
     </>
   )
